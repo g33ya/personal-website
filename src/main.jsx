94ec9839fd222510b14623944086e97fd76c3b20
g33ya <115582758+g33ya@users.noreply.github.com>
2026-06-29 16:50:25 -0400

    Preloading background images

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import corkboard from "./assets/experience/corkboard.webp";
+import deskBg from "./assets/skills/desk_texture.webp";
+
+const importantImages = [corkboard, deskBg];
+
+async function preloadImages(srcs) {
+  await Promise.all(
+    srcs.map(
+      (src) =>
+        new Promise((resolve) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve;
+          img.onerror = resolve;
+        })
+    )
+  );
+}
+
+preloadImages(importantImages).then(() => {
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
